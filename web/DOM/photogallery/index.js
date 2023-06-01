5cb0014b680d70165a8e14e9515663461e463035
AlvaroGReg <alvaro.gracia.reg@gmail.com>
2023-06-01 09:56:59 +0200

    fix

--- web/DOM/photogallery/index.js
+++ web/DOM/photogallery/index.js
@@ -1,11 +1,11 @@
 // Array de direcciones de imagen
-const images = ["img/1.jpg", "img/2.jpg", "img/3.jpg", "img/4.jpg", "img/5.jpg", "img/6.jpg", "img/7.jpg", "img/8.jpg"]
+const images = ['img/1.jpg', 'img/2.jpg', 'img/3.jpg', 'img/4.jpg', 'img/5.jpg', 'img/6.jpg', 'img/7.jpg', 'img/8.jpg']
 
 var currentIndex = 0
-const imageElement = document.getElementById("image")
-const prevButton = document.getElementById("prevBtn")
-const nextButton = document.getElementById("nextBtn")
-const thumbnailContainer = document.getElementById("thumbnailContainer")
+const imageElement = document.getElementById('image')
+const prevButton = document.getElementById('prevBtn')
+const nextButton = document.getElementById('nextBtn')
+const thumbnailContainer = document.getElementById('thumbnailContainer')
 var timer
 
 /**
@@ -16,26 +16,26 @@ var timer
  * imageElement's src attribute with it.
  */
 const changeImage = (index) => {
-	currentIndex = index
-	imageElement.src = images[currentIndex]
-	resetTimer()
-	updateThumbnails()
+  currentIndex = index
+  imageElement.src = images[currentIndex]
+  resetTimer()
+  updateThumbnails()
 }
 
 const previousImage = () => {
-	currentIndex = currentIndex === 0 ? images.length - 1 : currentIndex - 1
-	changeImage(currentIndex)
+  currentIndex = currentIndex === 0 ? images.length - 1 : currentIndex - 1
+  changeImage(currentIndex)
 }
 
 const nextImage = () => {
-	currentIndex = currentIndex === images.length - 1 ? 0 : currentIndex + 1
-	changeImage(currentIndex)
+  currentIndex = currentIndex === images.length - 1 ? 0 : currentIndex + 1
+  changeImage(currentIndex)
 }
 
 // reset timer
 const resetTimer = () => {
-	clearInterval(timer)
-	timer = setInterval(nextImage, 2000)
+  clearInterval(timer)
+  timer = setInterval(nextImage, 2000)
 }
 
 /**
@@ -43,31 +43,32 @@ const resetTimer = () => {
  * to a container.
  */
 const updateThumbnails = () => {
-	thumbnailContainer.innerHTML = ""
-	for (var i = 0; i < images.length; i++) {
-		var thumbnail = document.createElement("img")
-		thumbnail.src = images[i]
-		thumbnail.classList.add("thumbnail")
-		if (i === currentIndex) {
-			thumbnail.classList.add("current")
-		}
-		thumbnail.addEventListener("click", thumbnailClickHandler(i))
-		thumbnailContainer.appendChild(thumbnail)
-	}
+  thumbnailContainer.innerHTML = ''
+  for (let i = 0; i < images.length; i++) {
+    const thumbnail = document.createElement('img')
+    thumbnail.src = images[i]
+    thumbnail.classList.add('thumbnail')
+
+    if (i === currentIndex) {
+		thumbnail.classList.add('current')
+    }
+    thumbnail.addEventListener('click', thumbnailClickHandler(i))
+    thumbnailContainer.appendChild(thumbnail)
+  }
 }
 
 const thumbnailClickHandler = (index) => {
-	return function() {
-	  changeImage(index);
-	};
+  return function () {
+    changeImage(index)
   }
+}
 
-prevButton.addEventListener("click", function () {
-	previousImage()
+prevButton.addEventListener('click', function () {
+  previousImage()
 })
 
-nextButton.addEventListener("click", function () {
-	nextImage()
+nextButton.addEventListener('click', function () {
+  nextImage()
 })
 
 resetTimer()
